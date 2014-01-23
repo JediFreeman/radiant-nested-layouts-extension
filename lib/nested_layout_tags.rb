2c7aff4a36477b4e8dfdf8b4934f66d8ce147ebd
Ben Evans <jedifreeman@gmail.com>
2014-01-23 11:10:41 -0800

    add fallback when body part is missing

--- lib/nested_layout_tags.rb
+++ lib/nested_layout_tags.rb
@@ -70,7 +70,7 @@ module NestedLayoutTags
     tag.globals.nested_layouts_content_stack ||= []
     
     # return the saved content if any, or mimic a default +<r:content/>+ tag (render the body part)
-    tag.globals.nested_layouts_content_stack.pop || tag.globals.page.render_snippet(tag.locals.page.part('body'))
+    tag.globals.nested_layouts_content_stack.pop || tag.globals.page.render_snippet( ( tag.locals.page.part('body') || '' ) )
   end
   
   desc %{
